be9d4adff90df27e157de35b7a6981d048a1f6f5
Chris Ashfield <biodieselchris@gmail.com>
2020-09-22 10:01:42 -0700

    Change static pages views and homepage render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "hello Aki and O-chan!"
+    render html: "What this is: Marathon training plans and advise for real runners. 
+                  What this isn't: crybaby facebook shit."
   end
 end
