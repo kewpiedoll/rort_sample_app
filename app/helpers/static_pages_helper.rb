d1b1e4c6855920ea2463f2dbe669c7f1ada656de
Chris Ashfield <biodieselchris@gmail.com>
2020-09-23 11:40:33 -0700

    Finish static pages

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,11 @@
 module StaticPagesHelper
+
+  def page_title(title)
+    base_title = "American Runner"
+    if title.present?
+      full_title = title.to_s + " | " + base_title
+    else
+      base_title
+    end
+  end
 end
